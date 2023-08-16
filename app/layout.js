b0f38a772fbefb5130e6af4bd35365c307a21e1c
ayethandar <ayethandar@abank.com.mm>
2023-08-17 00:23:49 +0630

    third commit

--- app/layout.js
+++ app/layout.js
@@ -23,13 +23,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="max-w-6xl mx-auto p-4">
-          <AuthProvider>
+          {/* <AuthProvider> */}
             <QueryClientProvider client={queryClient}>
               <Header />
               {children}
               <EmployeeList />
             </QueryClientProvider>
-          </AuthProvider>
+          {/* </AuthProvider> */}
         </div>
       </body>
     </html>
