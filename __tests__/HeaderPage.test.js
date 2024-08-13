96863f5277beeffeb7ff411299ad70af0ac21a31
ayethandar <ayethandar@abank.com.mm>
2024-08-13 11:00:43 +0630

    Add unit test

---
+++ __tests__/HeaderPage.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Header from "@/components/Header";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Header />);
+
+    const heading = screen.getByRole("heading", {
+      name: "Employee Management",
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
